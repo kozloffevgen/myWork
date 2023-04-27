34980e68896d504411276651ec2020e5ed0a5452
Evgen <inevitability.k@gmail.com>
2023-04-27 14:49:13 +0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <MenuCatalogs />
+    <CardsCatalog />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuCatalogs from './components/MenuCatalogs.vue';
+import CardsCatalog from './components/CardsCatalog.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CardsCatalog,
+    MenuCatalogs,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MenuCatalogs.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu-catalogs">
+    <h2 class="menu-catalogs__title">Меню каталогов</h2>
+
+    <div class="menu-catalogs__settings">
+      
+    </div>
+    <div class="menu-catalogs__container">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MenuCatalog',
+  };
+</script>
+
+<style lang="less" scoped>
+.menu-catalogs {
+  
+}
+</style>
